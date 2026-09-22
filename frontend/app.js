dff196f2ab812f8c5fecbeb0b4efc273356bd792
zeem developer <mklovers13@gmail.com>
2026-09-22 16:39:22 +0500

    Update app.js

--- frontend/app.js
+++ frontend/app.js
@@ -27,7 +27,7 @@ const products = [
     category: "Samosas",
     price: 40,
     unit: "each",
-    image: "Chicken Samosa.jpg",
+    image: "chicken samosa.jpg",
     description: "Crispy chicken-filled samosa."
   },
 
